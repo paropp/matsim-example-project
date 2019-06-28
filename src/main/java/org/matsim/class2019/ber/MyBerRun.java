1bfebda50cdd3d2033aa478056f83cbed5a71372
misax <misax@misax-Lenovo-G780>
2019-06-28 15:00:14 +0200

    beginning config2java

--- src/main/java/org/matsim/class2019/ber/MyBerRun.java
+++ src/main/java/org/matsim/class2019/ber/MyBerRun.java
@@ -1,5 +1,8 @@
 package org.matsim.class2019.ber;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import org.matsim.api.core.v01.Scenario;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
@@ -12,21 +15,33 @@ import org.matsim.core.replanning.strategies.DefaultPlanStrategiesModule.Default
 import org.matsim.core.scenario.ScenarioUtils;
 
 public class MyBerRun {
+	
+	private static final Path BASE_PATH = Paths.get( "/home/misax/Documents/berlin-v5.3-10pct_BER/" ) ;
+	private static final Path INPUT_PATH = BASE_PATH.resolve( "input" ) ;
+	private static final Path EDITS_PATH = BASE_PATH.resolve( "edits" ) ;
+	
+	private static final String COORDINATE_SYSTEM = "GK4" ;
+	private static final long RANDOM_SEED = 2342 ;
+	private static final int NUMBER_OF_ITERATIONS = 500 ;
+	private static final int WRITE_INTERVAL = 100 ;
 
 	public static void main( String[] args ) {
 		String configfile = "./scenarios/equil/config.xml" ;
-		Config config = ConfigUtils.loadConfig(configfile) ;
+		Config config = ConfigUtils.loadConfig( configfile ) ;
 		
-		config.controler().setLastIteration( 2 );
+		config.controler().setLastIteration( NUMBER_OF_ITERATIONS );
 		config.controler().setOverwriteFileSetting( OverwriteFileSetting.overwriteExistingFiles ) ;
-		config.controler().setRunId( "berlin-v5.3-10pct-niceField" );
+		config.controler().setRunId( "berlin-v5.3-10pct-BER-superTrain" );
 		config.controler().setOutputDirectory( "./output" ) ;
-		config.controler().setWriteEventsInterval( 100 ) ;
-		config.controler().setWritePlansInterval( 100 ) ;
+		config.controler().setWriteEventsInterval( WRITE_INTERVAL ) ;
+		config.controler().setWritePlansInterval( WRITE_INTERVAL ) ;
 		
-		config.global().setCoordinateSystem( coordinateSystem ) ;
+		config.global().setCoordinateSystem( COORDINATE_SYSTEM ) ;
 		config.global().setInsistingOnDeprecatedConfigVersion( false ) ;
-		config.global().setNumberOfThreads( 4 );
+		config.global().setNumberOfThreads( NUMBER_OF_THREADS );
+		config.global().setRandomSeed( RANDOM_SEED );
+		
+		config.network().setInputFile( EDITS_PATH.resolve( "" ) );
 
 //		// new mode
 //		StrategySettings stratSets = new StrategySettings() ;
