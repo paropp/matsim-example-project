c65e77262b47718c7e15fd319e42bdbb194f0430
misax <misax@desktop>
2019-07-18 21:14:09 +0200

    pre run 4

--- src/main/java/org/matsim/class2019/ber/RunBer.java
+++ src/main/java/org/matsim/class2019/ber/RunBer.java
@@ -203,14 +203,14 @@ public class RunBer {
 		// EDITS
 		//////////
 		//cause files referenced in config were changed
-		config.network().setInputFile( OUTPUT_NETWORK_PATH.toString() );
-		config.transit().setVehiclesFile( OUTPUT_VEHICLES_PATH.toString() );
-		config.transit().setTransitScheduleFile( OUTPUT_TRANSIT_SCHEDULE_PATH.toString() );
-		config.plans().setInputFile( OUTPUT_PLANS_PATH.toString() );
-		config.controler().setOutputDirectory( BASE_PATH.resolve( "output" ).toString() );
-		config.controler().setLastIteration( 0 ); 
-		//config.controler().setWriteEventsInterval( 50 ) ;
-		//config.controler().setWritePlansInterval( 50 ) ;
+		config.network().setInputFile( OUTPUT_NETWORK_PATH.toString() ) ;
+		config.transit().setVehiclesFile( OUTPUT_VEHICLES_PATH.toString() ) ;
+		config.transit().setTransitScheduleFile( OUTPUT_TRANSIT_SCHEDULE_PATH.toString() ) ;
+		config.plans().setInputFile( OUTPUT_PLANS_PATH.toString() ) ;
+		config.controler().setOutputDirectory( BASE_PATH.resolve( "output" ).toString() ) ;
+		config.controler().setLastIteration( 300 ) ; 
+		config.controler().setWriteEventsInterval( 50 ) ;
+		config.controler().setWritePlansInterval( 50 ) ;
 		
 		// activities:
 		for ( long ii = 600 ; ii <= 97200; ii+=600 ) {
